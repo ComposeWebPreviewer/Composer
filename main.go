4ee7fcf74b0f3a6370bf2962629f87f62827031a
Tanish Azad <tanishazad03@gmail.com>
2025-04-30 03:31:27 +0530

    add default s3 code

--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"encoding/base64"
 	"fmt"
 	"os"
 	"os/exec"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func main() {
-	encodedCode := os.Args[1]
+	uploadToS3()
+}
 
+func decodeCode(encodedCode string) string {
 	decodedCode, _ := base64.StdEncoding.DecodeString(encodedCode)
 	fmt.Println("Decoded Code")
 	fmt.Println(string(decodedCode))
 
-	buildComposableBinaries(string(decodedCode))
+	return string(decodedCode)
 }
 
 func buildComposableBinaries(snippet string) error {
@@ -36,4 +44,28 @@ func buildComposableBinaries(snippet string) error {
     fmt.Println(string(stdout))
 
 	return nil
+}
+
+func uploadToS3() {
+	bucket := os.Getenv("S3_BUCKET")
+	key := "example.txt"
+	content := "Hello from ECS task!"
+
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		fmt.Println("Unable to load SDK config")
+	}
+
+	client := s3.NewFromConfig(cfg)
+
+	_, err = client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   bytes.NewReader([]byte(content)),
+	})
+	if err != nil {
+		fmt.Println("failed to upload")
+	}
+
+	fmt.Println("Upload successful!")
 }
